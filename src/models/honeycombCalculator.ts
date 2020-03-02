c2dee94b3f5e8f0a2cae49b6188ea44b2ee27817
Jack <jack@jackson.lan>
2020-03-02 05:30:35 +0100

    small fix

--- src/models/honeycombCalculator.ts
+++ src/models/honeycombCalculator.ts
@@ -3,10 +3,16 @@ class HoneycombCalculator {
     private readonly edgeLength: number;
     private readonly margin: number;
     private readonly offset: { x: number, y: number };
+    private readonly xl: number;
+    private readonly yl: number;
     public constructor(edgeLength: number, margin: number, x: number = 0, y: number = 0) {
         this.edgeLength = edgeLength;
         this.margin = margin;
         this.offset = { x: x, y: y };
+        const d1 = 0.86602540378 * 2 * this.edgeLength + this.margin;
+        this.xl = 0.86602540378 * d1;
+        this.yl = 0.5 * d1;
+
     }
     /**
      * calculate center position for honeycomb
@@ -15,24 +21,18 @@ class HoneycombCalculator {
      */
     public getCenter(col: number, row: number): { x: number, y: number } {
         const center = { x: this.offset.x, y: this.offset.y };
-        const d1 = 0.86602540378 * 2 * this.edgeLength + this.margin;
-        const xl = 0.86602540378 * d1;
-        const yl = 0.5 * d1;
         let offY = 0;
         let offX = 0;
         //even row
         if (row % 2 === 0) {
-            offX += col * 2 * xl;
-            offY += Math.floor(row / 2) * 2 * yl;
-            console.log("even");
+            offX += col * 2 * this.xl;
+            offY += Math.floor(row / 2) * 2 * this.yl;
         }
         //odd row
         else {
-            offX += 2 * col * xl + xl;
-            offY += Math.floor(row / 2) * 2 * yl + yl;
-            console.log("odd");
+            offX += 2 * col * this.xl + this.xl;
+            offY += Math.floor(row / 2) * 2 * this.yl + this.yl;
         }
-        console.log(`(${row}.${col}): off x=${offX} y=${offY}`)
         center.x += offX;
         center.y += offY;
         return center;
